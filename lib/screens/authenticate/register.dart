6bc21e7fe8b03b1b5cb2ea56cb1d12de9db75f99
Chris <ChrisNosowsky@gmail.com>
2020-05-05 12:20:47 -0400

    Sign in and register with firebase authentication

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_brew/services/auth.dart';
+import 'package:coffee_brew/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -11,10 +12,12 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password= '';
+  String error = '';
 
 
   @override
@@ -38,16 +41,21 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                validator: (val) => val.isEmpty ? "Enter an email" : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                validator: (val) => val.length < 6 ? "Enter a password with 6 or more characters" : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -63,10 +71,21 @@ class _RegisterState extends State<Register> {
                   ),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
-
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    if(result == null) {
+                      setState(() => error = 'Please supply a valid email');
+                    }
+                  }
                 },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0
+                ),
               )
             ],
           ),
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_brew/services/auth.dart';
+import 'package:coffee_brew/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,10 +13,12 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -38,16 +41,21 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Email"),
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Password"),
+                validator: (val) => val.length < 6 ? 'Enter a password with 6 or more characters' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -63,9 +71,21 @@ class _SignInState extends State<SignIn> {
                   ),
                 ),
                 onPressed: () async {
-                    print(email);
-                    print(password);
+                  if(_formKey.currentState.validate()) {
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if(result == null) {
+                      setState(() => error = "Invalid credentials");
+                    }
+                  }
                 }
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0
+                ),
               )
             ],
           ),
