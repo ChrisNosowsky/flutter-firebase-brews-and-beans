b42faec14cae19f69381fcc0ad7882e6e053c8ec
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-07-05 23:13:11 -0400

    Document created links with uid in Firestore backend

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_brew/models/user.dart';
+import 'package:coffee_brew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -47,6 +48,9 @@ class AuthService {
     try {
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
+
+      // create a new document for the user and the uid
+      await DatabaseService(uid: user.uid).updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch(e) {
       print(e.toString());
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+
+  final String uid;
+  DatabaseService({ this.uid });
+
   //collection reference
   final CollectionReference theBrewCollection = Firestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await theBrewCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength
+    });
+  }
+
 }
